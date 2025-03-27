1b6af1a263e54bce8454e2f459177e7ea37bf81d
rottedfm <rottedfm@proton.me>
2025-03-27 13:59:22 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,47 +1,96 @@
-# .system/flake.nix
-
 {
-  description = "!system!"; 
+  description = "!system! with Home Manager, Stylix, and DevShell";
 
   inputs = {
-    # nixpkgs stable
+    # Stable nixpkgs
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    # nixpkgs unstable
+
+    # Unstable nixpkgs
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    # home-manager
+
+    # Home Manager
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    # stylix
+    # Stylix theming
     stylix.url = "github:danth/stylix/release-24.11";
+
+    # Rust overlay
+    rust-overlay.url = "github:oxalica/rust-overlay";
+
+    # Utility helpers
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, home-manager, stylix, ... }@inputs:
+  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, home-manager, stylix, rust-overlay, flake-utils, ... }:
     let
-      inherit (self) outputs;
-      # supported systems
-      systems = [ "aarch64-linux" "i686-linux" "x86_64-linix" ];
+      overlays = import ./overlays { inherit inputs; };
+    in flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "i686-linux" ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (import rust-overlay)
+          ];
+        };
 
-      forAllSystems = nixpkgs.lib.genAttrs systems;
+        rust = pkgs.rust-bin.stable.latest.default.override {
+          targets = [ "x86_64-unknown-linux-musl" ];
+        };
+      in {
+        # `nix develop` shell
+        devShells.default = pkgs.mkShell {
 
-    in {
-      overlays = import ./overlays {inherit inputs; };
+          shellHook = '''
+            exec zsh
+          ''';
+          buildInputs = [
+            rust
+            pkgs.rust-analyzer
+            pkgs.rustfmt
+            pkgs.clippy
+            pkgs.lldb_19
+            pkgs.bacon
+            pkgs.openssl
+            pkgs.xdotool
 
-      # sudo nixos-rebuild switch --flake .#fm
+            (pkgs.python3.withPackages(p: with p; [
+              rich
+            ]))
+          ];
+
+          RUST_BACKTRACE = "1";
+          RUST_SRC_PATH = "${rust}/lib/rustlib/src/rust/library";
+        };
+      }) // {
+
+      # Global overlays available to all systems
+      overlays = overlays;
+
+      # NixOS configuration for system `fm`
       nixosConfigurations = {
         fm = nixpkgs.lib.nixosSystem {
           specialArgs = { inherit inputs outputs; };
-          modules = [ stylix.nixosModules.stylix ./system/configuration.nix ];
+          modules = [
+            stylix.nixosModules.stylix
+            ./system/configuration.nix
+          ];
         };
       };
 
-      # home-manager switch --flake .#rotted@fm
+      # Home Manager configuration for user `rotted@fm`
       homeConfigurations = {
         "rotted@fm" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux; # TODO: Make this for all systems
+          pkgs = import nixpkgs {
+            system = "x86_64-linux"; # Update if needed per system
+          };
           extraSpecialArgs = { inherit inputs outputs; };
-          modules = [ stylix.homeManagerModules.stylix ./user/home.nix ];
+          modules = [
+            stylix.homeManagerModules.stylix
+            ./user/home.nix
+          ];
         };
       };
     };
 }
+
