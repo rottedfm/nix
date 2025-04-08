71598d9dca74901f8ca85f4d73980e874d628167
rottedfm <rottedfm@proton.me>
2025-04-08 09:53:24 -0400

    Auto-commit: 2025-04-08 09:53:24

--- modules/home/librewolf.nix
+++ modules/home/librewolf.nix
@@ -1,22 +1,7 @@
 { ... }:
 
 {
-  programs.firefox = {
+  programs.librewolf = {
     enable = true;
-    policies = {
-      DisableTelemetry = true;
-      DisableFirefoxStudies = true;
-      Preferences = {
-        "cookiebanners.service.mode.privateBrowsing" = 2;
-        "cookiebanners.service.mode" = 2;
-        "privacy.donottrackheader.enabled" = true;
-        "privacy.fingerprintingProtection" = true;
-        "privacy.resistFingerprinting" = true;
-        "privacy.trackingprotection.emailtracking.enabled" = true;
-        "privacy.trackingprotection.enabled" = true;
-        "privacy.trackingprotection.fingerprinting.enabled" = true;
-        "privacy.trackingprotection.socialtracking.enabled" = true;    
-      };
-    };
   };
 }
