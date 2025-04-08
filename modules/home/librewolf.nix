15387927248d15b373fdc47c40e9a4c2bdb2a66a
rottedfm <rottedfm@proton.me>
2025-04-08 09:47:24 -0400

    Auto-commit: 2025-04-08 09:47:24

---
+++ modules/home/librewolf.nix
@@ -0,0 +1,32 @@
+{ ... }:
+
+{
+  programs.firefox = {
+    enable = true;
+    policies = {
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      Preferences = {
+        "cookiebanners.service.mode.privateBrowsing" = 2;
+        "cookiebanners.service.mode" = 2;
+        "privacy.donottrackheader.enabled" = true;
+        "privacy.fingerprintingProtection" = true;
+        "privacy.resistFingerprinting" = true;
+        "privacy.trackingprotection.emailtracking.enabled" = true;
+        "privacy.trackingprotection.enabled" = true;
+        "privacy.trackingprotection.fingerprinting.enabled" = true;
+        "privacy.trackingprotection.socialtracking.enabled" = true;    
+      };
+    };
+    ExtensionSettings = {
+      "jid1-ZAdIEUB7XOzOJw@jetpack" = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/duckduckgo-for-firefox/latest.xpi";
+        installation_mode = "force_installed";
+      };
+      "uBlock0@raymondhill.net" = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+        installation_mode = "force_installed";
+      };
+    };
+  };
+}
