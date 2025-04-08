c82a78a1828bda21e635f04b9b7daa6ab0a73183
rottedfm <rottedfm@proton.me>
2025-04-07 20:06:42 -0400

    Auto-commit: 2025-04-07 20:06:42

--- modules/home/niri.nix
+++ modules/home/niri.nix
@@ -17,7 +17,7 @@ layout {
 
   focus-ring {
     width 4
-    active-gradient from="#ff79c6" to="#71461" angle=45
+    active-gradient from="#ff79c6" to="#714161" angle=45
     inactive-gradient from="#31182d" to="#201010" angle=45 relative-to="workspace-view"
   }
 
