7f19e8b49ac721c21b8593aae43c0f20c957da19
rottedfm <rottedfm@proton.me>
2025-04-08 20:03:17 -0400

    Auto-commit: 2025-04-08 20:03:17

--- modules/nixos/fonts.nix
+++ modules/nixos/fonts.nix
@@ -10,27 +10,25 @@
 
 
   console = {
-    font = "ter-v32b";
-    earlySetup = true;
-      colors = [
-        "101010" 
-        "e0e0e0" 
-        "101010" 
-        "e64747" 
-        "50fa7b" 
-        "ffb86c" 
-        "b294bb" 
-        "815f8d" 
-        "d1afdd" 
-        "e0e0e0" 
-        "201010" 
-        "ff79c6" 
-        "50fa7b" 
-        "ffb86c" 
-        "b294bb" 
-        "ffcfff" 
-        "d1afdd" 
-        "efefef" 
+    colors = [
+      "101010" 
+      "e0e0e0" 
+      "101010" 
+      "e64747" 
+      "50fa7b" 
+      "ffb86c" 
+      "b294bb" 
+      "815f8d" 
+      "d1afdd" 
+      "e0e0e0" 
+      "201010" 
+      "ff79c6" 
+      "50fa7b" 
+      "ffb86c" 
+      "b294bb" 
+      "ffcfff" 
+      "d1afdd" 
+      "efefef" 
     ];
   };
 }
