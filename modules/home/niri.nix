d0b1e4b1a87ccc406321e79ccd6356e8d2b2502e
rottedfm <rottedfm@proton.me>
2025-04-14 11:35:00 -0400

    Auto-commit: 2025-04-14 11:35:00

--- modules/home/niri.nix
+++ modules/home/niri.nix
@@ -10,6 +10,7 @@
 
   programs.niri = {
     enable = true;
+    package = pkgs.niri-unstable;
     config = ''
 
 spawn-at-startup "swww-daemon"
@@ -57,7 +58,7 @@ binds {
 
   Mod+R { focus-column-first; }
 
-  Mod+F { fullscreen-window; }
+  Mod+F { toggle-windowed-fullscreen; }
 
   Mod+S { screenshot-screen; }
   Mod+Shift+S { screenshot-window; }
