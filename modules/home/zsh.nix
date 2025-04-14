794bc2fca35c9dac17edb03b06091842d4592b61
rottedfm <rottedfm@proton.me>
2025-04-14 12:22:04 -0400

    Auto-commit: 2025-04-14 12:22:04

--- modules/home/zsh.nix
+++ modules/home/zsh.nix
@@ -25,9 +25,12 @@ minecraft_wayland() {
   export SDL_VIDEODRIVER=wayland
   export QT_QPA_PLATFORM=wayland
   export LIBGL_ALWAYS_SOFTWARE=0
-  export MESA_NO_ERROR=1
   export _JAVA_AWT_WM_NONREPARENTING=1
-  export WLR_NO_HARDWARE_CURSORS=1
+  export __GLX_VENDOR_LIBRARY_NAME=mesa
+  export LIBGL_ALWAYS_INDIRECT=0
+  export GALLIUM_DRIVER=radeonsi
+  export LIBGL_DRI3_DISABLE=0
+  export MESA_LOADER_DRIVER_OVERRIDE=radeonsi
 
   local glfw_path
   glfw_path=$(nix eval --raw nixpkgs#glfw-wayland)/lib/libglfw.so.3
