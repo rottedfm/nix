afc708d6d11cb4c9f6f7612164e3cd2c9740ad71
rottedfm <rottedfm@proton.me>
2025-04-15 17:15:11 -0400

    Auto-commit: 2025-04-15 17:15:11

--- modules/nixos/zsh.nix
+++ modules/nixos/zsh.nix
@@ -7,7 +7,6 @@
   programs.zsh = {
     enable = true;
     shellAliases = {
-      sync = "nixpush";
       cd = "z";
       xmrig = "xmrig --donate-level 5 -o pool.supportxmr.com:443 -u 48UEoFuv5n9Hweg5JioChG4aX1jXPQtYcKtUqfry2iWe8HgqJHVztPYCqvQD8aX5f6CzG8jcXbkJMFexcKvkTRMsGEyhr5U -k --tls";
       run = "/home/rotted/Scripts/run/target/release/run";
